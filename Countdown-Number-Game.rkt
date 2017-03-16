3dd8de31127029c70c3bbf47155b7ab67cd9cf96
Alna132 <alanna@Alannas-MacBook-Pro.local>
2017-03-16 17:55:51 +0000

    Added some comments to the rkt file

--- Countdown-Number-Game.rkt
+++ Countdown-Number-Game.rkt
@@ -9,22 +9,26 @@
 ;(remove-duplicates (permutations usableNum))
 ;(remove-duplicates (permutations ops))
 
+; This gets a list of all the unique permutations of the ops and usableNum lists.
 (define all-numbers (remove-duplicates (permutations usableNum)))
 (define all-ops (cartesian-product ops ops ops ops ops))
 ; This generates too many nodes in the list.
 ;(cartesian-product all-ops all-numbers)
 
-(define start-perm (list -1 -1 -1 -1 1 1 1 1))
-(define X (remove-duplicates (permutations start-perm)))
-(define make-rpn l)
-(append (list 1 1) l (list -1))
+; Code from class to test out RPN in racket.
+; 2 1s are added to the front of the list and a -1 is added to the end.
+; This represents RPN as from right to left RPN must start with an operator and end with 2 numbers to be operated on.
+;(define start-perm (list -1 -1 -1 -1 1 1 1 1))
+;(define X (remove-duplicates (permutations start-perm)))
+;(define make-rpn l)
+;(append (list 1 1) l (list -1))
 
-(map make-rpn X)
+;(map make-rpn X)
 
-(define (valid-rpn? e [s 0])
-  (if (null? e)
-    (if (= s 1) #t #f)
-    (if (= (car e) 1)
-        (valid-rpn? (cdr e) (+ 1 s))
+;(define (valid-rpn? e [s 0])
+;  (if (null? e)
+;    (if (= s 1) #t #f)
+;    (if (= (car e) 1)
+;        (valid-rpn? (cdr e) (+ 1 s))
         ;(Call valid-rpn then (- 1 s))
-        )))
+;        )))
