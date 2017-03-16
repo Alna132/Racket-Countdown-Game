906d145d80780301c34a5c3620b42267a4fe9eb7
Alna132 <alanna@Alannas-MacBook-Pro.local>
2017-03-16 16:47:28 +0000

    Added reverse Polish notation code from class

--- Countdown-Number-Game.rkt
+++ Countdown-Number-Game.rkt
@@ -13,3 +13,18 @@
 (define all-ops (cartesian-product ops ops ops ops ops))
 ; This generates too many nodes in the list.
 ;(cartesian-product all-ops all-numbers)
+
+(define start-perm (list -1 -1 -1 -1 1 1 1 1))
+(define X (remove-duplicates (permutations start-perm)))
+(define make-rpn l)
+(append (list 1 1) l (list -1))
+
+(map make-rpn X)
+
+(define (valid-rpn? e [s 0])
+  (if (null? e)
+    (if (= s 1) #t #f)
+    (if (= (car e) 1)
+        (valid-rpn? (cdr e) (+ 1 s))
+        ;(Call valid-rpn then (- 1 s))
+        )))
