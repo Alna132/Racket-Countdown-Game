85fb6c7122c14274ed76f30db85bba8ca8a5ef44
Alna132 <alanna@Alannas-MacBook-Pro.local>
2017-03-16 16:11:22 +0000

    Added a test Cartesian-product function

--- Countdown-Number-Game.rkt
+++ Countdown-Number-Game.rkt
@@ -1,10 +1,15 @@
 #lang racket
 
 ; Variables
-(define nums (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+;(define nums (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
 (define targetNum 425)
 (define usableNum (list 100 50 25 19 1 1))
 (define ops (list '+ '- '* '/))
 
-(remove-duplicates (permutations usableNum))
-(remove-duplicates (permutations ops))
+;(remove-duplicates (permutations usableNum))
+;(remove-duplicates (permutations ops))
+
+(define all-numbers (remove-duplicates (permutations usableNum)))
+(define all-ops (cartesian-product ops ops ops ops ops))
+; This generates too many nodes in the list.
+;(cartesian-product all-ops all-numbers)
