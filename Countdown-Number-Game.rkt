eb36363ee10446ce3038e5e1de27ff9733f807e3
Alna132 <alanna@Alannas-MacBook-Pro.local>
2017-03-16 15:52:35 +0000

    Unique permutations of the ops and usableNum lists now display

--- Countdown-Number-Game.rkt
+++ Countdown-Number-Game.rkt
@@ -3,10 +3,8 @@
 ; Variables
 (define nums (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
 (define targetNum 425)
-(define L (list 100 50 25 19 1 1))
+(define usableNum (list 100 50 25 19 1 1))
 (define ops (list '+ '- '* '/))
 
-nums
-targetNum
-list
-ops
+(remove-duplicates (permutations usableNum))
+(remove-duplicates (permutations ops))
