1d5e50a053fc31eed80e340f9ee27d1e3c9ff55d
Alna132 <alanna@Alannas-MacBook-Pro.local>
2017-03-16 15:11:15 +0000

    Corrected definitions

--- Countdown-Number-Game.rkt
+++ Countdown-Number-Game.rkt
@@ -1,6 +1,12 @@
 #lang racket
 
-(define ops ('+ '- '* '/))
-(define nums (1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
-(define targetNum (425))
-(define list (100 50 25 19 1 1))
+; Variables
+(define nums (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+(define targetNum 425)
+(define L (list 100 50 25 19 1 1))
+(define ops (list '+ '- '* '/))
+
+nums
+targetNum
+list
+ops
